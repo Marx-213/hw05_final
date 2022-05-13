2a0b7196c0016bf69040ab53f6926ee8b63d2afe
Латыф Маркс <osammarks@gmail.com>
2022-05-14 01:15:22 +0600

    project6-4

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
 ]
 
 handler404 = 'core.views.page_not_found'
+handler500 = 'core.views.internal_server_error'
 if settings.DEBUG:
     urlpatterns += static(
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
